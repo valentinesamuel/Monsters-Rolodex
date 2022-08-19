b8b181bd85f69771cb008d2c8cf0543ddb305e9f
valentinesamuel <valentinesamuel2580@gmail.com>
2022-08-19 17:10:24 +0100

    FEAT: Migrated app to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
+import { getData } from './utils/fetch.utils';
+export type Monster = {
+  id: string;
+  name: String;
+  email: string;
+}
+
 const App = () => {
 
-  const [searchField, setSearchField] = useState('')
-  const [monsters, setMonsters] = useState([])
+  const [searchField, setSearchField] = useState<string>('')
+  const [monsters, setMonsters] = useState<Monster[]>([])
   const [filteredMonsters, setFilteredMonsters] = useState(monsters)
 
   useEffect(() => {
+   
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>("https://jsonplaceholder.typicode.com/users")
+      setMonsters(users)
+    }
 
-
-    fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.json()).then((users) => setMonsters(users))
-
+fetchUsers()
   }, [])
 
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>):void => {
     const searchFieldString = event.target.value.toLowerCase()
     setSearchField(searchFieldString)
   }
 
   useEffect(() => {
- const    newFilteredMonsters = monsters.filter(monster => {
+    const newFilteredMonsters = monsters.filter(monster => {
       return monster.name.toLowerCase().includes(searchField)
     })
     setFilteredMonsters(newFilteredMonsters)
@@ -41,14 +51,5 @@ const App = () => {
 
 }
 
-
-
-
-
-//     return (
-
-//     );
-//   }
-// }
 
 export default App;
--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,14 +1,16 @@
+import { Monster } from "../../App";
 import CardContainer from "../card-container/card-container.component";
 import './card-list.styles.css'
 
-const CardList = ({ monsters }) => {
-
+type CardListProps = {
+      monsters: Monster[];
+}
+const CardList = ({ monsters }:CardListProps) => {
 
       return <div className="card-list">
             {
                   monsters.map(monster => {
-                        const { name, email, id } = monster
-                        return <CardContainer key={id} name={name} email={email} id={id} />
+                        return <CardContainer key={monster.id} monster={monster} />
                   })
             }
       </div>;
